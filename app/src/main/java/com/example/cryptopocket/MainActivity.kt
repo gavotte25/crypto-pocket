7b8f377f302224372179f7293783e7b84decfd17
Phuc <gavotte.tran@gmail.com>
2021-06-02 11:32:41 +0700

    format theme color

--- app/src/main/java/com/example/cryptopocket/MainActivity.kt
+++ app/src/main/java/com/example/cryptopocket/MainActivity.kt
@@ -2,31 +2,10 @@ package com.example.cryptopocket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import com.example.cryptopocket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
-    private lateinit var appBarConfiguration: AppBarConfiguration
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        navController = findNavController(R.id.nav_host_fragment)
-        val drawerLayout = binding.drawerLayout
-        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
-        val navView = binding.navView
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
-        NavigationUI.setupWithNavController(navView, navController)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        NavigationUI.navigateUp(navController, appBarConfiguration)
-        return super.onSupportNavigateUp()
+        setContentView(R.layout.activity_main)
     }
 }
